cd774ba15c5bdf1927217c0be5cf2979620d4470
copacartagenajessenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2026-06-13 16:17:18 -0400

    Update 04.productos2.php

--- 04.productos2.php
+++ 04.productos2.php
@@ -243,14 +243,14 @@ h3 {
    <li><a href="02.inicio.php">Inicio</a></li>
    <li><a href="03.productos.php" >Cuidado</a></li>
    <li><a href="04.productos2.php"class="activo">Cosmeticos</a></li>
-   <li><a href="05.Acercade.php">Nosotros</a></li>
+   <li><a href="05.acercade.php">Nosotros</a></li>
    </ul>
   </nav>
 
 
   <div class="iconos-barra">
    <a href="09.register.php"><i class="fa-solid fa-user"></i> <span style="font-size:14px; margin-left:5px;"></span></a>
-   <a href="formProductos.php"><i class="fa-solid fa-bag-shopping"></i></a>
+   <a href="16.formproductos.php"><i class="fa-solid fa-bag-shopping"></i></a>
   </div>
 </header>
 
@@ -258,37 +258,37 @@ h3 {
 <h2 id="titulo1"></h2>
 <div class="caja" id="caja1">
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr1.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr1.jpeg" alt="" class="">
     <h3>Desmaquillante de Chic</h3>
     <h3>Codigo: PRD-023</h3>
     <h3>Precio: 28 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr34.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr34.jpeg" alt="" class="">
     <h3>Desmaquillante de Manzanilla y Avena</h3>
     <h3>Codigo: PRD-024</h3>
     <h3>Precio: 30 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr33.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr33.jpeg" alt="" class="">
        <h3>Desmaquillante de Uva Morada</h3>
     <h3>Codigo: PRD-025</h3>
     <h3>Precio: 32 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr31.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr31.jpeg" alt="" class="">
     <h3>Desmaquillante de Pepino</h3>
     <h3>Codigo: PRD-026</h3>
     <h3>Precio: 27 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr32.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr32.jpeg" alt="" class="">
     <h3>Desmaquillante Leche de Coco</h3>
     <h3>Codigo: PRD-027</h3>
     <h3>Precio: 29 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr30.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr30.jpeg" alt="" class="">
     <h3>Desmaquillante de Agua de Rosas</h3>
     <h3>Codigo: PRD-028</h3>
     <h3>Precio: 28 Bs</h3></div>
@@ -299,31 +299,31 @@ h3 {
 <h2 id="titulo2"></h2>
 <div class="caja" id="caja2">
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr12.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr12.jpeg" alt="" class="">
     <h3>Balsamo de Castaña</h3>
     <h3>Codigo: PRD-029</h3>
     <h3>Precio: 20 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr27.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr27.jpeg" alt="" class="">
     <h3>Balsamo de Frambuesa</h3>
     <h3>Codigo: PRD-030</h3>
     <h3>Precio: 20 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr28.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr28.jpeg" alt="" class="">
     <h3>Balsamo de Maracuya</h3>
     <h3>Codigo: PRD-031</h3>
     <h3>Precio: 20 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr29.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr29.jpeg" alt="" class="">
     <h3>Balsamo de Vainilla y Coco</h3>
     <h3>Codigo: PRD-032</h3>
     <h3>Precio: 22 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr26.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr26.jpeg" alt="" class="">
     <h3>Balsamo de Frutilla</h3>
     <h3>Codigo: PRD-033</h3>
     <h3>Precio: 20 Bs</h3></div>
@@ -335,31 +335,31 @@ h3 {
 <h2 id="titulo3"></h2>
 <div class="caja" id="caja3">
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr13.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr13.jpeg" alt="" class="">
     <h3>Perfume Solido de Orquidea</h3>
     <h3>Codigo: PRD-034</h3>
     <h3>Precio: 35 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr35.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr35.jpeg" alt="" class="">
     <h3>Perfume Solido de Bergamota</h3>
     <h3>Codigo: PRD-035</h3>
     <h3>Precio: 35 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr36.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr36.jpeg" alt="" class="">
     <h3>Perfume Solido de Frutilla y Petalos de Rosa</h3>
     <h3>Codigo: PRD-036</h3>
     <h3>Precio: 38 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr37.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr37.jpeg" alt="" class="">
     <h3>Perfume Solido de Vainilla y Flores Blancas</h3>
     <h3>Codigo: PRD-037</h3>
     <h3>Precio: 38 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr39.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr39.jpeg" alt="" class="">
     <h3>Perfume Solido de Jazmin</h3>
     <h3>Codigo: PRD-038</h3>
     <h3>Precio: 35 Bs</h3></div>
@@ -371,31 +371,31 @@ h3 {
 <h2 id="titulo4"></h2>
 <div class="caja" id="caja4">
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr17.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr17.jpeg" alt="" class="">
     <h3>Polvo Maiz Morado</h3>
     <h3>Codigo: PRD-039</h3>
     <h3>Precio: 18 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr40.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr40.jpeg" alt="" class="">
     <h3>Polvo Arcilla Rosada</h3>
     <h3>Codigo: PRD-040</h3>
     <h3>Precio: 20 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr42.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr42.jpeg" alt="" class="">
     <h3>Polvo Te Verde Molido</h3>
     <h3>Codigo: PRD-041</h3>
     <h3>Precio: 18 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr41.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr41.jpeg" alt="" class="">
     <h3>Polvo Avena Coloidal</h3>
     <h3>Codigo: PRD-042</h3>
     <h3>Precio: 17 Bs</h3></div>
     </a>
 
-    <a href="formProductos.php"><div class="producto"><img src="./img/zpr43.jpeg" alt="" class="">
+    <a href="16.formproductos.php"><div class="producto"><img src="./img/zpr43.jpeg" alt="" class="">
     <h3>Polvo de Remolacha</h3>
     <h3>Codigo: PRD-043</h3>
     <h3>Precio: 18 Bs</h3></div>

3884b7adaf7cae22d6c484256f5c23a84c732da4
scherecardoso <aranzacardoso5@gmail.com>
2026-06-16 14:33:09 -0400

    ghmjhg

---
+++ footer.php
@@ -0,0 +1,94 @@
+<style>
+    footer {
+  grid-area: pie;
+  padding:60px;
+  }
+
+.contenedor-pie {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 40px;
+  margin-bottom: 40px;
+  }
+
+.seccion-pie {
+  min-width: 250px;
+  }
+
+.seccion-pie h4 {
+  font-family: 'Playfair Display', serif;
+  font-size: 18px;
+  color: #000000;
+  margin-bottom: 10px;
+  font-weight: 600;
+  }
+
+.seccion-pie p {
+  color: #000000;
+  font-weight: 400;
+  font-size: 14px;
+  }
+
+.iconos-redes {
+  display: flex;
+  gap: 20px;
+  margin-top: 10px;
+  }
+
+.iconos-redes a {
+  color: #2b2b2b;
+  font-size: 18px;
+  transition: color 0.3s ease;
+  }
+
+@media {
+     footer {
+    padding: 30px 20px;
+    height: auto;
+  }
+
+
+  .contenedor-pie {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    gap: 25px;
+  }
+
+
+  .seccion-pie {
+    width: 100%;
+  }
+
+
+  .seccion-pie h4 {
+    font-size: 16px;
+  }
+
+
+  .seccion-pie p {
+    font-size: 13px;
+  }
+
+
+  .iconos-redes {
+    justify-content: center;
+  }
+}
+</style>
+
+<footer>
+  <div class="contenedor-pie">
+ <div class="seccion-pie"></div> 
+
+  <div class="seccion-pie">
+   <h4>Síguenos</h4>
+  <div class="iconos-redes">
+    <a ><i class="fab fa-instagram"></i></a>
+    <a ><i class="fab fa-tiktok"></i></a>
+    <a ><i class="fab fa-facebook-f"></i></a>
+  </div>
+  </div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,91 @@
+<style>
+header {
+  grid-area: barra;
+  background-color: #ffffffb5;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 120px;
+  top: 0;
+  position: sticky;
+  height: 60px;
+  z-index: 100;
+}
+
+.logo {
+  font-family: 'Playfair Display', serif;
+  font-size: 13px;
+  color: #000000;
+  margin: 0;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  gap: 40px;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  gap: 25px;
+  margin: 0;
+  padding: 0;
+}
+
+nav a {
+  text-decoration: none;
+  color: #2b2b2b;
+  font-weight: 500;
+  font-size: 15px;
+  position: relative;
+}
+
+nav a.activo::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -6px;
+  width: 100%;
+  height: 1px;
+  background-color: #333333;
+}
+
+.iconos-barra {
+  display: flex;
+  align-items: center;
+  gap: 25px;
+}
+
+.iconos-barra a {
+  color: #2b2b2b;
+  text-decoration: none;
+  font-size: 18px;
+  position: relative;
+}
+</style>
+
+<header>
+  <div class="logo">
+    <h1>Natué</h1>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="02.inicio.php" class="activo">Inicio</a></li>
+      <li><a href="03.productos.php">Cuidado</a></li>
+      <li><a href="04.productos2.php">Cosmeticos</a></li>
+      <li><a href="05.acercade.php">Nosotros</a></li>
+    </ul>
+  </nav>
+
+  <div class="iconos-barra">
+    <a href="09.register.php">
+      <i class="fa-solid fa-user"></i>
+    </a>
+
+    <a href="16.formproductos.php">
+      <i class="fa-solid fa-bag-shopping"></i>
+    </a>
+  </div>
+</header>

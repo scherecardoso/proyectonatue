c16cd55479eea688fe57e64aa982e2e1e49886bc
copacartagenajessenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2026-06-16 21:13:52 -0400

    sfdsgfhg

---
+++ 28.formcarrito.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" id="formusuarios">
+
+    <input type="number" name="CI" placeholder="CI" required>
+    <input type="text" name="nombre" placeholder="Nombre" required>
+    <input type="text" name="direccion" placeholder="Dirección" required>
+    <input type="number" name="celular" placeholder="Celular" required>
+    <button type="submit">Registrar</button>
+</form>
+</body>
+</html>

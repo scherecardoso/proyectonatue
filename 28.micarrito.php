dd8fceb8d0966caf94a59da9f30cb51cb3d4912d
copacartagenajessenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2026-06-17 20:24:20 -0400

    carrito

--- 28.micarrito.php
+++ 28.micarrito.php
@@ -1,3 +1,4 @@
+```php
 <?php
 $servidor = "localhost";
 $usuario = "root";
@@ -12,7 +13,7 @@ if ($conn->connect_error) {
 
 session_start();
 
-$id = isset($_GET['id']) ? $_GET['id'] : 0;
+$id = $_GET['id'];
 
 $sql = "SELECT * FROM productos";
 $result = $conn->query($sql);
@@ -28,16 +29,15 @@ if ($total == null) {
 }
 
 echo "<h1>Carrito de compras</h1>";
-echo "<h3>Total acumulado: Bs. $total</h3>";
 
-echo "<table border='1'>";
+echo "<table>";
 
 echo "<tr>
         <th>Codigo</th>
         <th>Precio</th>
-        <th>Cantidad Disponible</th>
-        <th>Agregar al carrito</th>
         <th>Cantidad</th>
+        <th>Agregar al carrito</th>
+        <th>Cantidad a comprar</th>
         <th>Accion</th>
       </tr>";
 
@@ -47,27 +47,23 @@ while ($fila = $result->fetch_assoc()) {
 
     echo "<tr>";
 
-    echo "<td>" . $fila['codigo'] . "</td>";
-    echo "<td>" . $fila['precio'] . "</td>";
-    echo "<td>" . $fila['cantidad'] . "</td>";
+    echo "<td>".$fila["codigo"]."</td>";
+    echo "<td>".$fila["precio"]."</td>";
+    echo "<td>".$fila["cantidad"]."</td>";
 
     echo "<td>
-            <a href='29.agregarcarrito.php?codigo=" . $fila['codigo'] . "'>
-                Agregar al carrito
+            <a href='29.agregarcarrito.php?codigo=".$fila['codigo']."'>
+            Agregar al carrito
             </a>
           </td>";
 
-    echo "<input type='hidden' name='codigo' value='" . $fila['codigo'] . "'>";
-    echo "<input type='hidden' name='id' value='" . $id . "'>";
-    echo "<input type='hidden' name='precio' value='" . $fila['precio'] . "'>";
+    echo "<input type='hidden' name='codigo' value='".$fila['codigo']."'>";
+    echo "<input type='hidden' name='id' value='".$id."'>";
+    echo "<input type='hidden' name='precio' value='".$fila['precio']."'>";
 
-    echo "<td>
-            <input type='number' name='cantidad' value='1' min='1'>
-          </td>";
+    echo "<td><input type='number' name='cantidad' value='0'></td>";
 
-    echo "<td>
-            <input type='submit' value='Agregar'>
-          </td>";
+    echo "<td><input type='submit' value='Agregar'></td>";
 
     echo "</tr>";
 
@@ -76,11 +72,12 @@ while ($fila = $result->fetch_assoc()) {
 
 echo "</table>";
 
-echo "<br>";
+echo "<br><br>";
 
 echo "<a href='formpedidos.php'>
-        <button>Nuevo pedido</button>
+        <button>Nuevo Pedido</button>
       </a>";
 
 $conn->close();
 ?>
+```

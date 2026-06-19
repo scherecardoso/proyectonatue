f4f89f9a523bb6e3926a9a7009d80cc280af1ba0
copacartagenajesenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2026-06-19 15:35:50 -0400

    jcsdkhvbs

--- 28.micarrito.php
+++ 28.micarrito.php
@@ -9,19 +9,30 @@ $conn = new mysqli($servidor, $usuario, $contrasena, $bd);
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
-session_start();
-$pedidos_id=$_GET['pedidos_id'];
-$sql = "SELECT * FROM productos";
 
+session_start();
+
+$pedidos_id = $_GET['pedidos_id'];
+
+$sql = "SELECT * FROM productos";
 $resultado = $conn->query($sql);
-$sqlTotal="SELECT sum(costototal) FROM carrito where Pedido_id='$pedidos_id'";
-$costototal=$conn->query($sqlTotal);
-$res = $costototal->fetch_assoc();
-$costototal=$res['sum(costototal)'];
-if($res['sum(costototal)']==null){
-    $total=0;
+
+$sqlTotal = "SELECT SUM(costototal) FROM carrito WHERE pedidos_id='$pedidos_id'";
+$costototal = $conn->query($sqlTotal);
+
+if ($costototal == false) {
+    die("Error en la consulta: " . $conn->error);
 }
-echo "<h3>Total: ".$costototal."</h3>";
+
+$filaTotal = $costototal->fetch_assoc();
+$costototal = $filaTotal['SUM(costototal)'];
+
+if ($costototal == null) {
+    $costototal = 0;
+}
+
+echo "<h3>Total: " . $costototal . "</h3>";
+
 echo "<table border='1'>";
 
 echo "<tr>
@@ -30,34 +41,41 @@ echo "<tr>
         <th>Descripción</th>
         <th>Precio</th>
         <th>Acciones</th>
-        <th colspan=2>Agregar al Carrito</th>
+        <th colspan='2'>Agregar al Carrito</th>
       </tr>";
 
-while($fila = $resultado->fetch_assoc()){
+while ($fila = $resultado->fetch_assoc()) {
+
     echo "<form action='28.registrocarrito.php' method='post'>";
     echo "<tr>";
-        echo "<td>".$fila["codigo"]."</td>";
-        echo "<td>".$fila["nombre"]."</td>";
-        echo "<td>".$fila["descripcion"]."</td>";   
-        echo "<td>".$fila["precio"]."</td>";
-        echo "<td>
-                <a href='producto.php?productos_codigo=".$fila["productos_codigo"]."'>
-                    <button>Mostrar</button>
-                </a>
-            </td>";
-        echo "<input type='hidden' value=".$fila["productos_codigo"]." name='productos_codigo'>";
-        echo "<input type='hidden' value=".$pedidos_id." name='pedidos_id'>";
-        echo "<input type='hidden' value=".$fila["precio"]." name='precio'>";
-        echo "<td><input type='number' name='cantidad' value=0></td>";
-        echo "<td><input type='submit' value='Agregar'></td>";
-        echo "</tr>";
-        echo "</form>";
 
+    echo "<td>".$fila["codigo"]."</td>";
+    echo "<td>".$fila["nombre"]."</td>";
+    echo "<td>".$fila["descripcion"]."</td>";
+    echo "<td>".$fila["precio"]."</td>";
+
+    echo "<td>
+            <a href='22.readproductos.php?productos_codigo=".$fila["productos_codigo"]."'>
+                <button type='button'>Mostrar</button>
+            </a>
+          </td>";
+
+    echo "<input type='hidden' name='productos_codigo' value='".$fila["productos_codigo"]."'>";
+    echo "<input type='hidden' name='pedidos_id' value='".$pedidos_id."'>";
+    echo "<input type='hidden' name='precio' value='".$fila["precio"]."'>";
+
+    echo "<td><input type='number' name='cantidad' value='0'></td>";
+    echo "<td><input type='submit' value='Agregar'></td>";
+
+    echo "</tr>";
+    echo "</form>";
 }
 
 echo "</table>";
-echo "<a href='./pedios/1.formpedidos.php'>
+
+echo "<a href='./pedidos/1.formpedidos.php'>
         <button>Generar Nuevo Pedido</button>
       </a><br><br>";
 
+$conn->close();
 ?>
--- pedidos/1.formpedidos.php
+++ pedidos/1.formpedidos.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-    $vendedor=$_SESSION['nombre'];
+    $vendedor=$_SESSION['vendedor'];
     
 ?>
 

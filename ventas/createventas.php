20c99f513887533fc5e36be22ca6a076df16c286
angie <albabracamonteangie@pedropoveda.edu.bo>
2026-08-11 10:27:40 -0400

    u

--- ventas/createventas.php
+++ ventas/createventas.php
@@ -10,25 +10,40 @@ if ($conn->connect_error) {
     die("Conexion fallida: " . $conn->connect_error);
 }
 
-$costo = $_POST['total'];
+$costo = $_POST['pedidos_id'];
 $fecha = $_POST['fecha'];
 $estado = $_POST['estado'];
-$id_pedidos = $_POST['id_pedidos'];
+$id_pedidos = $_POST['total'];
 
-$sql = "INSERT INTO ventas (costo, fecha, estado, pedidos_id) 
-        VALUES ('$costo', '$fecha', '$estado', '$id_pedidos')";
+$sql = "INSERT INTO ventas (pedidos_id, fecha, estado, total) VALUES ('$pedidos_id', '$fecha', '$estado', '$total')";
 
-if($conn->query($sql)){
+if($conn->query($sql))
+  {
+      
+     //buscar nevamente los productos para desconectar stock
+$sql="SELECT * FROM micarrito WHERE pedidos_id='$pedidos_id'";
+$resultao=$conn->query($sql);
+while($fila=$resultao->fetch_assoc())
+    {
+          $codigo=$fila['productos_codigo'];
+         $cantidad=$fila['cantidad'];
+         //obtener stock actual 
 
-    $idVentas = $conn->insert_id;
+         $sql="SELECT * FROM producos WHERE codigo='$codigo'";
+         $r2=$conn->query($sql2);
+         $productos=$r2->fetch_assoc();
+         $stock=$productos['stock'];
+         $nuevosSTOCK=$stock-$cantidad;
+         //actualizar stock
+         $sql3="UPDATE producos SET stock=´$nuevoStock´ WHERE codigo=´$codigo´";
+         $cconn->query($sql3);
 
-    header("Location: readventas.php");
-    exit();
-
-}else{
-
-    echo $conn->error;
-
-}
-
+    }
+        echo "<h2>venta registrada correctmente</h2>";
+  }   
+   else
+    {
+        echo"error".$conn->error;
+    }
+    
 ?>

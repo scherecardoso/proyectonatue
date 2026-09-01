279e62010c57eed4a1f844c3f2f5465ecadde2e6
copacartagenajesenia-beep <copacartagenajessenia@pedropoveda.edu.bo>
2026-09-01 14:30:35 -0400

    sdgvefbh

--- usuario/08.usuario.php
+++ usuario/08.usuario.php
@@ -408,8 +408,8 @@ i{
 
 <section class="cards">
     <article class="card"><div class="icono"><i class="fa-solid fa-cart-shopping"></i></div><h3><?php echo $totalpedido; ?></h3><p>Pedidos</p></article>
-    <article class="card"><a href="../favoritos/favoritos.php"><div class="icono"><i class="fa-solid fa-heart"></i></div><h3>8</h3><p>Favoritos</p></a></article>
-    <article class="card"><div class="icono"><i class="fa-solid fa-star"></i></div><h3>5</h3><p>Reseñas</p></article>
+    <article class="card"><a href="../favoritos/favoritos.php"><div class="icono"><i class="fa-solid fa-heart"></i></div><p>Favoritos</p></a></article>
+    <article class="card"><a href="../pagina/revisar.php"><div class="icono"><i class="fa-solid fa-star"></i></div><p>Comentarios</p></a></article>
 </section>
 
 <section class="contenido">
